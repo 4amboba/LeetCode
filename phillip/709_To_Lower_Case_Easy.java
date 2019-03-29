467d2818394ccb7564929891e3010ce87c496fbc
4amboba <pchang0010@gmail.com>
2019-03-29 12:55:44 -0700

    Added solution for 709. To Lower Case - Easy

---
+++ phillip/709_To_Lower_Case_Easy.java
@@ -0,0 +1,21 @@
+// 709. To Lower Case - Easy
+
+// Initial thoughts: 
+// This seems laughably easy with java's toLowerCase() function
+
+// Implementation:
+
+class Solution {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
+
+// Afterthoughts:
+// Lol.
+
+// Results:
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for To Lower 
+// Case.
+// Memory Usage: 35.5 MB, less than 92.41% of Java online submissions for To 
+// Lower Case.
