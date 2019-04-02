67b35d25a28723c3fad2c9be2fb2f093e4cbd843
4amboba <pchang0010@gmail.com>
2019-04-01 16:20:55 -0800

    Added solution for 595. Big Countries - Easy

---
+++ phillip/595_Big_Countries_Easy.sql
@@ -0,0 +1,8 @@
+# Solution:
+SELECT name, population, area
+FROM world
+WHERE population > 25000000 OR area > 3000000;
+
+# Results:
+# Runtime: 198 ms, faster than 82.86% of MySQL online submissions for Big Countries.
+# Memory Usage: N/A
